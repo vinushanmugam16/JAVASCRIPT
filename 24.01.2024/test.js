72ac6dbbd5ab6bdd938b6d9fecdc68e963a7cae2
vinushanmugam16 <vinu.shanmugam@aspiresys.com>
2024-01-25 20:54:06 +0530

    24.01.2024

--- 24.01.2024/test.js
+++ 24.01.2024/test.js
@@ -4,3 +4,13 @@ console.log("Welcome to aspire " + name);
 
 const numbers=[1,3,4,7,99,4,0];
 console.log(numbers.filter((item)=>(item%2==0)));
+
+
+
+const person ='Paul';
+console.log('Hello ' + person);
+console.log('Hello '.concat(person));
+console.log(`Hello ${person}`);
+
+console.log(['Hello, Greetings'].join(" "));
+
